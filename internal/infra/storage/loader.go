6e89f3efa5cb156d207832b6628c21b7a9a1a24b
tnsoftbear <avi-team@inbox.lv>
2024-08-16 20:01:54 +0300

    -

--- internal/infra/storage/loader.go
+++ internal/infra/storage/loader.go
@@ -47,6 +47,7 @@ func (rl *RedisLoader) LoadCustomerById(ctx context.Context, id string) (*model.
 		if err := addressHGetAll.Err(); err != nil {
 			return nil, err
 		}
+		
 		addressRes, err := addressHGetAll.Result()
 		if err != nil {
 			return nil, err
